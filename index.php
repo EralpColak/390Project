ed26539d197474776fccd404517c683b2d389318
eyyupcanisler <eyyupcanisler1@gmail.com>
2024-03-26 16:03:02 +0300

    red color

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <style>
         body {
             font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
+            background-color: red;
             margin: 0;
             padding: 0;
         }
